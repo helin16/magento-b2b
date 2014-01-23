6147b12c57421048303c7822dc7bef79951b11ac
lhe <lhe@bytecraft.com.au>
2014-01-23 18:00:17 +1100

    added order invoice view

--- web/protected/pages/Order/Details/OrderDetailsController.js
+++ web/protected/pages/Order/Details/OrderDetailsController.js
@@ -3,5 +3,17 @@
  */
 var PageJs = new Class.create();
 PageJs.prototype = Object.extend(new BPCPageJs(), {
-	
+	init: function() {
+		$$('.operationWrapper .operation_box .operation_title.expandable').each(function(item){
+			item.observe('click', function(){
+				if(item.hasClassName('collapsed')) {
+					item.up('.operation_box').down('.operation_content').show();
+					item.removeClassName('collapsed');
+				} else {
+					item.up('.operation_box').down('.operation_content').hide();
+					item.addClassName('collapsed');
+				}
+			});
+		});
+	}
 });
--- web/protected/pages/Order/Details/OrderDetailsController.php
+++ web/protected/pages/Order/Details/OrderDetailsController.php
@@ -13,6 +13,11 @@ class OrderDetailsController extends BPCPageAbstract
 	 * @see BPCPageAbstract::$menuItem
 	 */
 	public $menuItem = 'order';
+	/**
+	 * The order that we are viewing
+	 * 
+	 * @var Order
+	 */
 	public $order = null;
 	/**
 	 * (non-PHPdoc)
@@ -36,6 +41,9 @@ class OrderDetailsController extends BPCPageAbstract
 	protected function _getEndJs()
 	{
 		$js = parent::_getEndJs();
+		$js .= "pageJs.init();";
+		if(isset($this->Request['action']) && trim($this->Request['action']) === 'print')
+			$js .= "$$('.hidewhenprint').each(function(item){item.hide();});";
 		return $js;
 	}
 	/**
@@ -80,6 +88,31 @@ class OrderDetailsController extends BPCPageAbstract
 		$html .= $address->getStreet() . '<br />';
 		$html .= $address->getCity() . ' ' . $address->getRegion() . ' ' . $address->getPostCode() . '<br />';
 		$html .= $address->getCountry();
+		return $html;
+	}
+	public function getPaymentSummary()
+	{
+		$total = $this->order->getTotalAmount();
+		$totalNoGST = $total / 1.1;
+		$gst = $total - $totalNoGST;
+		$html = $this->_getPaymentSummaryRow('Total:', '$' . number_format($totalNoGST, 2, '.', ','), 'grandTotalNoGST');
+		$html .= $this->_getPaymentSummaryRow('GST:', '$' . number_format($gst, 2, '.', ','), 'gst');
+		$html .= $this->_getPaymentSummaryRow('Total(inc-GST):', '$' . number_format($total, 2, '.', ','), 'grandTotal');
+		$html .= $this->_getPaymentSummaryRow('Paid to Date:', '$' . number_format($this->order->getTotalPaid(), 2, '.', ','), 'paidTotal');
+		$overDueClass = $this->order->getTotalDue() > 0 ? 'overdue' : '';
+		$html .= $this->_getPaymentSummaryRow('Balance Due:', '$' . number_format($this->order->getTotalDue(), 2, '.', ','), 'dueTotal ' . $overDueClass);
+		return $html;
+	}
+	private function _getPaymentSummaryRow($title, $content, $class='')
+	{
+		$html = '<div class="row ' . $class . '">';
+			$html .= '<span class="details_title inlineblock">';
+				$html .= $title;
+			$html .= '</span>';
+			$html .= '<span class="details_content inlineblock">';
+				$html .= $content;
+			$html .= '</span>';
+		$html .= '</div>';
 		return $html;
 	}
 }
