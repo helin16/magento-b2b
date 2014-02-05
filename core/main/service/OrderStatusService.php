b0800a4105731a0c59901d59705bfe93d9802fdf
mrahman <mrahman@bytecraft.com.au>
2014-02-05 16:24:26 +1100

    adding new service for OrderStatus

---
+++ core/main/service/OrderStatusService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * OrderStatusService
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class OrderStatusService extends BaseServiceAbastract
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("OrderStatus");
+    }
+}
+?>
