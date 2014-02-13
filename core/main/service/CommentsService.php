ccb16a8062916db467fb381eeee6afdd2c14408c
mrahman <mrahman@bytecraft.com.au>
2014-02-13 11:29:03 +1100

    adding new service for Comments Entity

---
+++ core/main/service/CommentsService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CommentsService
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class CommentsService extends BaseServiceAbastract
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("Comments");
+    }
+}
+?>
