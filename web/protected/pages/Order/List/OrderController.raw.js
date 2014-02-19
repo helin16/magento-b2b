7373e6849e250e056edb9a0960f0ad81531ffac4
helin16 <helin16@gmail.com>
2014-02-19 21:37:48 +1100

    change the order list to load 10

--- web/protected/pages/Order/List/OrderController.raw.js
+++ web/protected/pages/Order/List/OrderController.raw.js
@@ -6,7 +6,7 @@ PageJs.prototype = Object.extend(new BPCPageJs(), {
 	resultDivId: '' //the html id of the result div
 	,searchDivId: '' //the html id of the search div
 	,totalNoOfItemsId: '' //the html if of the total no of items
-	,_pagination: {'pageNo': 1, 'pageSize': 30} //the pagination details
+	,_pagination: {'pageNo': 1, 'pageSize': 10} //the pagination details
 	,_searchCriteria: {} //the searching criteria
 	,_infoTypes:{} //the infotype ids
 	,orderStatuses: [] //the order statuses object
