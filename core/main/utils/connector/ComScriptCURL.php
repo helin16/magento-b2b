56fd0710fef96dc689cc939f4cd9d0f9eb64e938
l he <helin16@gmail.com>
2014-03-06 20:33:44 +1100

    removing proxy from ComScripts

--- core/main/utils/connector/ComScriptCURL.php
+++ core/main/utils/connector/ComScriptCURL.php
@@ -19,7 +19,7 @@ class ComScriptCURL
 				CURLOPT_FILE    => $fp,
 				CURLOPT_TIMEOUT => $timeout, // set this to 8 hours so we dont timeout on big files
 				CURLOPT_URL     => $url
-				,CURLOPT_PROXY   => 'proxy.bytecraft.internal:3128'
+// 				,CURLOPT_PROXY   => 'proxy.bytecraft.internal:3128'
 		);
 		foreach($extraOpts as $key => $value)
 			$options[$key] = $value;
