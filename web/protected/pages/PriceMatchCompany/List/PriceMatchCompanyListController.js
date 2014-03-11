98251383f37a8a9dc8a4374fe1ef9eb16d7153ed
mrahman <mrahman@WSD6-MRAHMAN.bytecraft.internal>
2014-03-11 18:17:22 +1100

    adding controller for price match company

--- web/protected/pages/PriceMatchCompany/List/PriceMatchCompanyListController.js
+++ web/protected/pages/PriceMatchCompany/List/PriceMatchCompanyListController.js
@@ -6,6 +6,64 @@ PageJs.prototype = Object.extend(new BPCPageJs(), {
 	_resultDivId: '' // the id of the result div
 	,_pageInfo: {'pageNo': 1, 'pageSize': 30}
 	,_searchCriteria: ''
+	
+	,_generateEditOptions: function(editSpan) {
+		var tmp = {};
+		tmp.me = this;
+		
+		tmp.parentRow = $(editSpan).up('.pmc_row');
+		tmp.pmcId = tmp.parentRow.retrieve('id');
+		tmp.aliasTextSpan = tmp.parentRow.getElementsBySelector('.single_alias')[0];
+		tmp.aliasText = tmp.aliasTextSpan.innerHTML;
+		tmp.aliasTextSpan.update(new Element('input', {'type': 'text', 'class': 'alias_text_box'}).writeAttribute('value', tmp.aliasText) );
+		$(editSpan).up('.single_alias_option').update('')	
+			.insert({'bottom': new Element('span', {}).update('Save') })
+			.insert({'bottom': new Element('span', {}).update('Cancel') 
+				.observe('click', function() {
+					tmp.aliasObj = $(this).up('.pmc_row').retrieve('data');
+					$(this).up('.pmc_row').replace(tmp.me._getAliasRow(tmp.aliasObj));
+				})
+			});
+	}
+	
+	,_getAliasRow: function(alias) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.id = alias.id;
+		tmp.alias = alias.alias;
+		if(alias && tmp.alias !== '' && tmp.alias !== null && tmp.alias !== undefined) {
+			return new Element('div', {'class': 'pmc_row'}).store('data', alias)
+				.insert({'bottom': new Element('span', {'class': 'single_alias'}).update(tmp.alias) })
+				.insert({'bottom': new Element('span', {'class': 'single_alias_option'})
+					.insert({'bottom': new Element('span') 
+						.update('Edit')
+						.observe('click', function() {
+								tmp.me._generateEditOptions(this);
+							})
+					})
+				});
+		}
+	}
+		
+	,_generateSingleRowForPriceMatchCompany: function(item) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.item = item;
+		
+		tmp.rowDiv = new Element('div', {'class': 'row'});
+		tmp.aliasSpan = new Element('span', {'class': 'cell company_alias'});
+		
+		tmp.companyName = tmp.item.companyName;
+		tmp.item.companyAliases.each(function(alias) {
+			tmp.aliasSpan.insert({'bottom': tmp.me._getAliasRow(alias) })
+		});
+		
+		tmp.rowDiv
+			.insert({'bottom': new Element('span', {'class': 'cell company_name'}).update(tmp.companyName) })
+			.insert({'bottom': tmp.aliasSpan });
+		
+		return tmp.rowDiv;
+	}	
 		
 	,displayAllPriceMatchCompany: function() {
 		var tmp = {};
@@ -19,15 +77,21 @@ PageJs.prototype = Object.extend(new BPCPageJs(), {
 				try 
 				{
 					tmp.result = tmp.me.getResp(param, false, true);
+					tmp.result.items.each(function(item) {
+						tmp.row = tmp.me._generateSingleRowForPriceMatchCompany(item);
+						$(tmp.me._resultDivId).insert({'bottom': tmp.row });
+					});
 				} 
 				catch(e) 
 				{
-					alert(e);
+					console.debug(e);
+					//alert(e);
 				}
 				
 			}
 		});
 		
+		return tmp.me;
 	}
 	
 });
