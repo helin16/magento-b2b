7ed741ab4e914be2c55ccaa635ef0f2cc46f17eb
mrahman <mrahman@WSD6-MRAHMAN.bytecraft.internal>
2014-03-11 17:51:07 +1100

    adding new service for PriceMatchCompany

---
+++ core/main/service/PriceMatchCompanyService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * UserAccount service
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class PriceMatchCompanyService extends BaseServiceAbastract
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("PriceMatchCompany");
+    }
+}
+?>
