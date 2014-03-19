630b13a8a9778fe5a33b5729f26a1cd01ca9997e
lhe <lhe@bytecraft.com.au>
2014-03-19 17:03:14 +1100

    bug fixing

--- core/promotion/script/toggleShippingBillAddress.php
+++ core/promotion/script/toggleShippingBillAddress.php
@@ -1,12 +1,12 @@
 <?php
 require_once dirname(__FILE__) . '/../../main/bootstrap.php';
+Core::setUser(FactoryAbastract::service('UserAccount')->get(UserAccount::ID_SYSTEM_ACCOUNT));
 $sql = "select id, billingAddrId, shippingAddrId from `order`";
 $result = Dao::getResultsNative($sql, array(), PDO::FETCH_ASSOC);
-
 echo "==== start";
 Dao::$debug = true;
 foreach($result as $row)
 {
-	Dao::updateByCriteria(new DaoQuery('Order'), 'billingAddrId = ?, shippingAddrId = ?', 'where id = ?', array());
+	Dao::updateByCriteria(new DaoQuery('Order'), 'billingAddrId = ?, shippingAddrId = ?, updated = updated, updatedById = updatedById', 'where id = ?', array($row['shippingAddrId'], $row['billingAddrId'], $row['id']));
 }
 echo "==== end";
