db2a91abae0a13f435c1a5730769af43ccd1098a
l he <helin16@gmail.com>
2014-03-24 23:16:30 +1100

    created user details page

--- web/protected/pages/User/Details/UsersController.php
+++ web/protected/pages/User/Details/UsersController.php
@@ -19,83 +19,84 @@ class UsersController extends BPCPageAbstract
 	protected function _getEndJs()
 	{
 		$js = parent::_getEndJs();
-		$js .= 'pageJs.setResultDiv("resultDiv")';
-			$js .= '.setTotalUserCountDiv("totalUsers")';
-			$js .= '.setCallbackId("getRoles", "' . $this->getRolesBtn->getUniqueId() . '")';
-			$js .= '.getUsers($("searchBtn"), true);';
+		$js .= 'pageJs.setHtmlIDs("savePanel")';
+			$js .= '.setCallbackId("saveUser", "' . $this->saveUserBtn->getUniqueId() . '")';
+			$js .= '.setEditUrl("/useraccount/edit/{uid}.html")';
+			$js .= '.load(' . json_encode($this->_getUser()) . ', ' . json_encode($this->_getRoles()). ')';
+			$js .= ';';
 		return $js;
 	}
-	/**
-	 * Getting the list of users
-	 * 
-	 * @param unknown $sender
-	 * @param unknown $params
-	 * 
-	 * @throws Exception
-	 */
-	public function getUsers($sender, $param)
+	private function _getUser()
 	{
-		$results = $errors = array();
-		try
-		{
-			if(!isset($param->CallbackParameter->searchCriteria) || ($serachCriteria = trim($param->CallbackParameter->searchCriteria)) === '')
-				$serachCriteria = '';
-				
-			$pageNo = 1;
-			$pageSize = DaoQuery::DEFAUTL_PAGE_SIZE;
-			if(isset($param->CallbackParameter->pagination))
-			{
-				$pageNo = $param->CallbackParameter->pagination->pageNo;
-				$pageSize = $param->CallbackParameter->pagination->pageSize;
-			}
-			
-			$where = '`ua`.id != :sysId';
-			$params = array('sysId' => UserAccount::ID_SYSTEM_ACCOUNT);
-			if($serachCriteria !== '')
-			{
-				$query = FactoryAbastract::service('UserAccount')->getDao()->getQuery();
-				$query->eagerLoad("UserAccount.person", 'inner join', 'ord', '`p`.id = `ua`.personId and (`p`.firstName like :firstName and `p`.lastName like :lastName)');
-				$params['firstName'] = $serachCriteria . '%';
-				$params['lastName'] = $serachCriteria . '%';
-				$where .= ' OR `ua`.username like :username';
-				$params['username'] = $serachCriteria . '%';
-			}
-			
-			$users = FactoryAbastract::service('UserAccount')->findByCriteria($where, $params, true, $pageNo, $pageSize);
-			$results['pageStats'] = FactoryAbastract::service('UserAccount')->getPageStats();
-			$results['items'] = array();
-			foreach($users as $item)
-				$results['items'][] = $item->getJson();
-		}
-		catch(Exception $ex)
-		{
-			$errors[] = $ex->getMessage();
-		}
-		$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
+		$userAccount = null;
+		if(!isset($this->Request['action']) || ($method = trim($this->Request['action'])) === 'edit' && !($userAccount = FactoryAbastract::service('UserAccount')->get($this->Request['id'])) instanceof UserAccount)
+			throw new Exception('Invalid params!');
+		return $userAccount instanceof UserAccount ? $userAccount->getJson() : null;
 	}
-	/**
-	 * deactive a user
-	 * 
-	 * @param unknown $sender
-	 * @param unknown $params
-	 * 
-	 * @throws Exception
-	 */
-	public function deleteUser($sender, $param)
+	private function _getRoles()
+	{
+		$roles = array();
+		foreach(FactoryAbastract::service('Role')->findAll(true, null, DaoQuery::DEFAUTL_PAGE_SIZE, array('r.name' => 'asc')) as $role)
+			$roles[] = $role->getJson();
+		return $roles;
+	}
+	public function saveUser($sender, $params)
 	{
 		$results = $errors = array();
 		try
 		{
-			if(!isset($param->CallbackParameter->userId) || !($userAccount = FactoryAbastract::service('UserAccount')->get(trim($param->CallbackParameter->userId))) instanceof UserAccount)
-				throw new Exception("Invalid user account passed for deletion!");
-			$userAccount->setActive(false);
+			Dao::beginTransaction();
+			if(!isset($params->CallbackParameter->firstName) || ($firstName = trim($params->CallbackParameter->firstName)) === '')
+				throw new Exception('System Error: firstName is mandatory!');
+			if(!isset($params->CallbackParameter->lastName) || ($lastName = trim($params->CallbackParameter->lastName)) === '')
+				throw new Exception('System Error: lastName is mandatory!');
+			if(!isset($params->CallbackParameter->userName) || ($userName = trim($params->CallbackParameter->userName)) === '')
+				throw new Exception('System Error: userName is mandatory!');
+			if(!isset($params->CallbackParameter->roleid) || !($role = FactoryAbastract::service('Role')->get($params->CallbackParameter->roleid)) instanceof Role)
+				throw new Exception('System Error: role is mandatory!');
+			
+			$newpassword = trim($params->CallbackParameter->newpassword);
+			if(!isset($params->CallbackParameter->userid) || !($userAccount = FactoryAbastract::service('UserAccount')->get($params->CallbackParameter->userid)) instanceof UserAccount)
+			{
+				$userAccount = new UserAccount();
+				$person = new Person();
+				if($newpassword === '')
+					throw new Exception('System Error: new password is mandatory!');
+				$newpassword = sha1($newpassword);
+			}
+			else
+			{
+				$person = $userAccount->getPerson();
+				if($newpassword === '')
+					$newpassword = $userAccount->getPassword();
+				else
+					$newpassword = sha1($newpassword);
+			}
+			
+			//double check whether the username has been used
+			$users = FactoryAbastract::service('UserAccount')->findByCriteria('username=? and id!=?', array($userName, $userAccount->getId()), false, 1, 1);
+			if(count($users) > 0)
+				throw new Exception('Username(=' . $userName . ') has been used by another user, please choose another one!');
+			
+			$person->setFirstName($firstName)
+				->setLastName($lastName);
+			FactoryAbastract::service('Person')->save($person);
+			
+			$userAccount->setUserName($userName)
+				->setPassword($newpassword)
+				->setPerson($person);
 			FactoryAbastract::service('UserAccount')->save($userAccount);
-			$results = $userAccount->getJson();
+			
+			$results = $userAccount->clearRoles()
+				->addRole($role)
+				->getJson();
+			Dao::commitTransaction();
 		}
 		catch(Exception $ex)
 		{
+			Dao::rollbackTransaction();
 			$errors[] = $ex->getMessage();
 		}
-		$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
+		$params->ResponseData = StringUtilsAbstract::getJson($results, $errors);
 	}
 }
