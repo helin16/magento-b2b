234902281df9e880d78e71331d5ace05e3471ae5
mrahman <mrahman@bytecraft.com.au>
2014-03-28 16:52:09 +1100

    adding service for CourierInfoType and CourierInfo

---
+++ core/main/service/CourierInfoService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CourierInfoService
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class CourierInfoService extends BaseServiceAbastract
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("CourierInfo");
+    }
+}
+?>
---
+++ core/main/service/CourierInfoTypeService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CourierInfoTypeService
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class CourierInfoTypeService extends BaseServiceAbastract
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("CourierInfoType");
+    }
+}
+?>
