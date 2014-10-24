d184ffe97825eb55930cd37d2698d1717987e8b4
lhe <lhe@bytecraft.com.au>
2014-10-24 14:00:31 +1100

    changed Customer Js

--- web/protected/pages/Customer/List/ListController.1.js
+++ web/protected/pages/Customer/List/ListController.1.js
@@ -33,27 +33,30 @@ PageJs.prototype = Object.extend(new CRUDPageJs(), {
 	/**
 	 * Displaying the selected address 
 	 */
-	,_displaySelectedAddress: function(item) {
+	,_displaySelectedAddress: function(btn) {
 		var tmp = {};
 		tmp.me = this;
+		tmp.item = $(btn).up('[item_id]').retrieve('data');
+		jQuery('.popover-loaded').popover('hide');
 		//remove highlight
 		jQuery('.item_row.success').removeClass('success');
 		//mark this one as active
-		tmp.selectedRow = jQuery('[item_id=' + item.id + ']')
+		tmp.selectedRow = jQuery('[item_id=' + tmp.item.id + ']')
 			.addClass('success');
 		
-		if(!tmp.selectedRow.hasClass('popover-loaded')) {
-			tmp.selectedRow.popover({
-				'title'    : 'title',
+		tmp.me._signRandID(btn); //sign it with a HTML ID to commnunicate with jQuery
+		if(!jQuery('#' + btn.id).hasClass('popover-loaded')) {
+			jQuery('#' + btn.id).popover({
+				'container': 'body',
+				'title'    : '<div class="row"><div class="col-xs-10">title</div><div class="col-xs-2"><a class="pull-right" href="javascript:void(0);" onclick="jQuery(' + "'#" + btn.id + "'" + ').popover(' + "'hide'" + ');"><strong>&times;</strong></a></div></div>',
 				'html'     : true, 
 				'placement': 'right', 
-				'trigger'  : 'click', 
-				'content'  : '<p>test content</p>', 
+				'trigger'  : 'manual', 
+				'content'  : '<p>test content f;dsklfsdl;akfjasd;lfkj dsa;fl kwr;lk fdsafds</p>', 
 			})
 			.addClass('popover-loaded');
 		}
-		//tmp.selectedRow.popover('toggle');
-
+		jQuery('#' + btn.id).popover('toggle');
 		return tmp.me;
 	}
 	,_getResultRow: function(row, isTitle) {
@@ -61,7 +64,6 @@ PageJs.prototype = Object.extend(new CRUDPageJs(), {
 		tmp.me = this;
 		tmp.tag = (tmp.isTitle === true ? 'th' : 'td');
 		tmp.isTitle = (isTitle || false);
-		console.debug(row);
 		tmp.row = new Element('tr', {'class': (tmp.isTitle === true ? '' : 'btn-hide-row')}).store('data', row)
 			.insert({'bottom': new Element(tmp.tag, {'class': 'name col-xs-1'}).update(row.name) })
 			.insert({'bottom': new Element(tmp.tag, {'class': 'email col-xs-1'}).update(row.email) })
@@ -70,19 +72,25 @@ PageJs.prototype = Object.extend(new CRUDPageJs(), {
 			//.insert({'bottom': new Element(tmp.tag, {'class': 'address col-xs-2'}).update(row.address.billing.full) })
 			//glyphicon glyphicon-send
 			.insert({'bottom': tmp.isTitle === true ? row.addresses : new Element(tmp.tag, {'class': 'address col-xs-1'})
-				.insert({'bottom': (new Element('span', {'class': 'glyphicon glyphicon-plane address-shipping'})
-				.observe('click', function(){
-					console.debug('works');
-					tmp.me._displaySelectedAddress(row);
+				.insert({'bottom': new Element('span', {'style': 'display: inline-block'})
+					.insert({'bottom': new Element('a', {'class': 'visible-xs visible-md visible-sm visible-lg', 'href': 'javascript: void(0);'})
+						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-plane address-shipping'}) })
+						.observe('click', function(){
+							console.debug('works');
+							tmp.me._displaySelectedAddress(this);
+						})
+					})
 				})
-				) })
-				.insert({'bottom': (new Element('span', {'class': 'glyphicon glyphicon-usd address-billing'})
-				.observe('click', function(){
-					console.debug('works2');
+				.insert({'bottom': new Element('span', {'style': 'display: inline-block'})
+					.insert({'bottom':  new Element('a', {'class': 'visible-xs visible-md visible-sm visible-lg', 'href': 'javascript: void(0);'})
+						.insert({'bottom': new Element('span', {'class': 'glyphicon glyphicon-usd address-billing'}) })
+						.observe('click', function(){
+							console.debug('works2');
+							tmp.me._displaySelectedAddress(this);
+						})
+					})
 				})
-				) })
 			})
-			
 			.insert({'bottom': new Element(tmp.tag, {'class': 'address col-xs-2'}).update(row.address.shipping.full) })
 			.insert({'bottom': new Element(tmp.tag, {'class': 'mageId col-xs-1'}).update(row.mageId) })
 			.insert({'bottom': new Element(tmp.tag, {'class': 'cust_active col-xs-1'})
