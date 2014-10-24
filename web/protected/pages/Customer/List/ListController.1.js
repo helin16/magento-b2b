e0cfd16becdd4618c77000aeb9407fb6bfd94f1c
Frank <tlan16@sina.com>
2014-10-24 13:29:23 +1100

    popover not working

--- web/protected/pages/Customer/List/ListController.1.js
+++ web/protected/pages/Customer/List/ListController.1.js
@@ -4,8 +4,8 @@
 var PageJs = new Class.create();
 PageJs.prototype = Object.extend(new CRUDPageJs(), {
 	_getTitleRowData: function() {
-		return {'email': "Email", 'name': 'Name', 'contactNo': 'Contact Num', 'description': 'Description', 
-			'address': {'billing': {'full': 'Billing Address'}, 'shipping': {'full': 'shipping Address'} },
+		return {'email': "Email", 'name': 'Name', 'contactNo': 'Contact Num', 'description': 'Description', 'addresses': 'Addresses',
+			'address': {'billing': {'full': 'Billing Address'}, 'shipping': {'full': 'Shipping Address'} },
 			'mageId': "Mage Id", 'active': "Active?"
 			//mageId
 			};
@@ -30,7 +30,32 @@ PageJs.prototype = Object.extend(new CRUDPageJs(), {
 	,_getEditPanel: function(row) {
 
 	}
+	/**
+	 * Displaying the selected address 
+	 */
+	,_displaySelectedAddress: function(item) {
+		var tmp = {};
+		tmp.me = this;
+		//remove highlight
+		jQuery('.item_row.success').removeClass('success');
+		//mark this one as active
+		tmp.selectedRow = jQuery('[item_id=' + item.id + ']')
+			.addClass('success');
+		
+		if(!tmp.selectedRow.hasClass('popover-loaded')) {
+			tmp.selectedRow.popover({
+				'title'    : 'title',
+				'html'     : true, 
+				'placement': 'right', 
+				'trigger'  : 'click', 
+				'content'  : '<p>test content</p>', 
+			})
+			.addClass('popover-loaded');
+		}
+		//tmp.selectedRow.popover('toggle');
 
+		return tmp.me;
+	}
 	,_getResultRow: function(row, isTitle) {
 		var tmp = {};
 		tmp.me = this;
@@ -42,7 +67,22 @@ PageJs.prototype = Object.extend(new CRUDPageJs(), {
 			.insert({'bottom': new Element(tmp.tag, {'class': 'email col-xs-1'}).update(row.email) })
 			.insert({'bottom': new Element(tmp.tag, {'class': 'contact col-xs-1'}).update(row.contactNo)})
 			.insert({'bottom': new Element(tmp.tag, {'class': 'description col-xs-1'}).update(row.description) })
-			.insert({'bottom': new Element(tmp.tag, {'class': 'address col-xs-2'}).update(row.address.billing.full) })
+			//.insert({'bottom': new Element(tmp.tag, {'class': 'address col-xs-2'}).update(row.address.billing.full) })
+			//glyphicon glyphicon-send
+			.insert({'bottom': tmp.isTitle === true ? row.addresses : new Element(tmp.tag, {'class': 'address col-xs-1'})
+				.insert({'bottom': (new Element('span', {'class': 'glyphicon glyphicon-plane address-shipping'})
+				.observe('click', function(){
+					console.debug('works');
+					tmp.me._displaySelectedAddress(row);
+				})
+				) })
+				.insert({'bottom': (new Element('span', {'class': 'glyphicon glyphicon-usd address-billing'})
+				.observe('click', function(){
+					console.debug('works2');
+				})
+				) })
+			})
+			
 			.insert({'bottom': new Element(tmp.tag, {'class': 'address col-xs-2'}).update(row.address.shipping.full) })
 			.insert({'bottom': new Element(tmp.tag, {'class': 'mageId col-xs-1'}).update(row.mageId) })
 			.insert({'bottom': new Element(tmp.tag, {'class': 'cust_active col-xs-1'})
