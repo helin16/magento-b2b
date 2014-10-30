bb5d250bd1530b46db3ca303f48b27ae8e2545e7
Peter <peterwang@budgetpc.com.au>
2014-10-30 12:20:18 +1100

    db dump bat

---
+++ core/promotion/db_dump.bat
@@ -0,0 +1,4 @@
+:: Dump
+del .\bpcinternal_30Aug14\bpcinternal_30Aug14.sql
+e:\wamp\bin\mysql\mysql5.5.24\bin\mysqldump.exe -h localhost -u root -proot bpcinternal > .\bpcinternal_30Aug14\bpcinternal_30Aug14.sql
+Pause
