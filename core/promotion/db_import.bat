9f8a8a9d01029b3bbf2b623458c25dfaad288399
Frank <tlan16@sina.com>
2014-11-14 12:44:50 +1100

    db with PO

--- core/promotion/db_import.bat
+++ core/promotion/db_import.bat
@@ -8,5 +8,5 @@ c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot -e "CREATE DATABASE `
 Pause
 
 :: Import sql file
-c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot bpcinternal < .\bpcinternal_30Aug14\bpcinternal_30Aug14.sql
+c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -proot bpcinternal < .\bpcinternal.sql
 Pause
