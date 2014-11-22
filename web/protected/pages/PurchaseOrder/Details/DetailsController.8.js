5ce9816946d1244a17cbce77fae4cb2a2780b5a8
Frank <tlan16@sina.com>
2014-11-23 09:23:17 +1100

    before change PO detail page

--- web/protected/pages/PurchaseOrder/Details/DetailsController.8.js
+++ web/protected/pages/PurchaseOrder/Details/DetailsController.8.js
@@ -101,17 +101,11 @@ PageJs.prototype = Object.extend(new DetailsPageJs(), {
 				.insert({'bottom': new Element('strong', {'class': 'col-sm-4 pull-left'}).update('Purchase Order Info') })
 			})
 			.insert({'bottom': new Element('div', {'class': 'row'})
-				.insert({'bottom': new Element('div', {'class': 'col-sm-1 id'}).update(tmp.me._getFormGroup('ID', new Element('input', {'save-item': 'id', 'type': 'text', 'value': tmp.item.id ? tmp.item.id : ''}) ) ) })
+				.insert({'bottom': new Element('div', {'class': 'col-sm-1 purchaseRefNo'}).update(tmp.me._getFormGroup('PO RefNumber', new Element('input', {'save-item': 'purchaseRefNo', 'type': 'value', 'value': tmp.item.supplierRefNo ? tmp.item.supplierRefNo : ''}) ) ) })
 				.insert({'bottom': new Element('div', {'class': 'col-sm-1 purchaseOrderNo'}).update(tmp.me._getFormGroup('Mage ID', new Element('input', {'save-item': 'purchaseOrderNo', 'type': 'value', 'value': tmp.item.purchaseOrderNo ? tmp.item.purchaseOrderNo : ''}) ) ) })
 				.insert({'bottom': new Element('div', {'class': 'col-sm-1 active'}).update(tmp.me._getFormGroup('Active?', new Element('input', {'save-item': 'active', 'type': 'checkbox', 'checked': tmp.item.active }) ) ) })
 				.insert({'bottom': new Element('div', {'class': 'col-sm-2 orderDate'}).update(tmp.me._getFormGroup('Ordered Date', 
 							new Element('input', {'class': 'datepicker', 'save-item': 'orderDate', 'value': (tmp.item.orderDate ? tmp.item.orderDate : '') })  
-					) ) })
-				.insert({'bottom': new Element('div', {'class': 'col-sm-2 createDate'}).update(tmp.me._getFormGroup('Created Date', 
-							new Element('input', {'class': 'datepicker', 'save-item': 'created', 'value': (tmp.item.created ? tmp.item.created : '') })  
-					) ) })
-				.insert({'bottom': new Element('div', {'class': 'col-sm-2 updateDate'}).update(tmp.me._getFormGroup('Updated Date', 
-							new Element('input', {'class': 'datepicker', 'save-item': 'updated', 'value': (tmp.item.updated ? tmp.item.updated : '') })  
 					) ) })
 			})
 			.insert({'bottom': new Element('div', {'class': 'row'})
@@ -122,14 +116,8 @@ PageJs.prototype = Object.extend(new DetailsPageJs(), {
 				.insert({'bottom': new Element('div', {'class': 'col-sm-1 supplierId'}).update(tmp.me._getFormGroup('ID', new Element('input', {'save-item': 'supplierId', 'type': 'text', 'value': tmp.item.supplier.id ? tmp.item.supplier.id : ''}) ) ) })
 				.insert({'bottom': new Element('div', {'class': 'col-sm-1 supplierMageId'}).update(tmp.me._getFormGroup('Mage ID', new Element('input', {'save-item': 'supplierMageId', 'type': 'value', 'value': tmp.item.supplier.mageId ? tmp.item.supplier.mageId : ''}) ) ) })
 				.insert({'bottom': new Element('div', {'class': 'col-sm-1 supplierActive'}).update(tmp.me._getFormGroup('Active?', new Element('input', {'save-item': 'supplierActive', 'type': 'checkbox', 'checked': tmp.item.supplier.active }) ) ) })
-				.insert({'bottom': new Element('div', {'class': 'col-sm-1 supplierContactName'}).update(tmp.me._getFormGroup('contactName', new Element('input', {'save-item': 'supplierContactName', 'type': 'text', 'value': tmp.item.supplier.contactName ? tmp.item.supplier.contactName : '' }) ) ) })
-				.insert({'bottom': new Element('div', {'class': 'col-sm-1 supplierContactNo'}).update(tmp.me._getFormGroup('Contact No', new Element('input', {'save-item': 'supplierContactNo', 'type': 'value', 'value': tmp.item.supplier.contactNo ? tmp.item.supplier.contactNo : '' }) ) ) })
-				.insert({'bottom': new Element('div', {'class': 'col-sm-2 supplierCreateDate'}).update(tmp.me._getFormGroup('Created Date', 
-							new Element('input', {'class': 'datepicker', 'save-item': 'supplierCreated', 'value': (tmp.item.supplier.created ? tmp.item.supplier.created : '') })  
-					) ) })
-				.insert({'bottom': new Element('div', {'class': 'col-sm-2 supplierUpdateDate'}).update(tmp.me._getFormGroup('Updated Date', 
-							new Element('input', {'class': 'datepicker', 'save-item': 'supplierUpdated', 'value': (tmp.item.supplier.updated ? tmp.item.supplier.updated : '') })  
-					) ) })
+				.insert({'bottom': new Element('div', {'class': 'col-sm-1 supplierContactName'}).update(tmp.me._getFormGroup('contactName', new Element('input', {'save-item': 'supplierContactName', 'type': 'text', 'value': tmp.item.supplierContact ? tmp.item.supplierContact : '' }) ) ) })
+				.insert({'bottom': new Element('div', {'class': 'col-sm-1 supplierContactNo'}).update(tmp.me._getFormGroup('Contact No', new Element('input', {'save-item': 'supplierContactNo', 'type': 'value', 'value': tmp.item.supplierContactNumber ? tmp.item.supplierContactNumber : '' }) ) ) })
 			})
 			.insert({'bottom': new Element('div', {'class': 'row'})
 				.insert({'bottom': new Element('strong', {'class': 'col-sm-4 pull-left'}).update('Finance Info') })
