fda021afe0014e28e43259eaba89d3bc0ef33e42
helin16 <helin16@gmail.com>
2014-12-06 12:26:47 +1100

    roll back to  previous version

--- core/promotion/PO.sql
+++ core/promotion/PO.sql
@@ -1,4 +1,6 @@
+ALTER TABLE `purchaseorder` CHANGE `supplierContactId` `supplierContact` VARCHAR(100) NOT NULL DEFAULT '';
 ALTER TABLE `purchaseorder` ADD `supplierContactNumber` VARCHAR(100) NOT NULL DEFAULT '' AFTER `supplierContact`, ADD INDEX (`supplierContactNumber`) ;
+ALTER TABLE purchaseorder DROP INDEX supplierContactId;
 ALTER TABLE `purchaseorder` ADD INDEX(`supplierContact`);
 
 ALTER TABLE `purchaseorder` ADD `shippingCost` DOUBLE(10,4) UNSIGNED NOT NULL DEFAULT '0.0000' AFTER `supplierContactNumber`, ADD INDEX (`shippingCost`) ;
