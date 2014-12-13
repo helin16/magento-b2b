aa9b5903f9ac4d9e39ee112b61a488197cec800d
Frank <tlan16@sina.com>
2014-12-13 15:53:14 +1100

    Print Delivery Docket

---
+++ web/protected/pages/Order/PrintDeliveryDocket/OrderPrintController.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * This is the OrderDetailsController
+ * 
+ * @package    Web
+ * @subpackage Controller
+ * @author     lhe<helin16@gmail.com>
+ */
+class OrderPrintController extends BPCPageAbstract
+{
+	/**
+	 * (non-PHPdoc)
+	 * @see BPCPageAbstract::$menuItem
+	 */
+	public $menuItem = 'order';
+	/**
+	 * The order that we are viewing
+	 * 
+	 * @var Order
+	 */
+	public $order = null;
+	/**
+	 * (non-PHPdoc)
+	 * @see BPCPageAbstract::onLoad()
+	 */
+	public function onLoad($param)
+	{
+		parent::onLoad($param);
+		if(!$this->isPostBack)
+		{
+			$this->order = Order::get($this->Request['orderId']);
+			if(!$this->order instanceof Order)
+				die('Invalid Order!');
+		}
+	}
+	/**
+	 * Getting the tr for each row
+	 * @param unknown $qty
+	 * @param unknown $sku
+	 * @param unknown $name
+	 * @param unknown $uprice
+	 * @param unknown $tprice
+	 * @return string
+	 */
+	public function getRow($qty, $sku, $name, $uprice, $tprice, $rowClass="")
+	{
+		return "<tr class='$rowClass'><td class='qty'>$qty</td><td class='sku'>$sku</td><td class='name'>$name</td></tr>";
+	}
+	/**
+	 * 
+	 * @return string
+	 */
+	public function showProducts()
+	{
+		$html = '';
+		foreach($this->order->getOrderItems() as  $index => $orderItem)
+		{
+			$uPrice = '';
+			$tPrice = '';
+			$html .= $this->getRow($orderItem->getQtyOrdered(), $orderItem->getProduct()->getSku(), $orderItem->getProduct()->getname(), $uPrice, $tPrice, 'itemRow');
+		}
+		for ( $i = 12; $i > $index; $i--)
+		{
+			$html .= $this->getRow('&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', '&nbsp;', 'itemRow');
+		}
+		return $html;
+	}
+	public function getAddress($type)
+	{
+		$method = 'get' . ucfirst($type) . 'Addr';
+		$address = $this->order->$method();
+		if(!$address instanceof Address)
+			return '';
+		$html = $address->getContactName() . '<br />';
+		$html .= $address->getStreet() . '<br />';
+		$html .= $address->getCity() . ' ' . $address->getRegion() . ' ' . $address->getPostCode() . '<br />';
+		$html .= $address->getCountry();
+		return $html;
+	}
+	public function getPaymentSummary()
+	{
+		$total = $this->order->getTotalAmount();
+		$totalNoGST = $total / 1.1;
+		$gst = $total - $totalNoGST;
+		$html = $this->_getPaymentSummaryRow('<strong>Receiving Info</strong>', '');
+		$html .= $this->_getPaymentSummaryRow('<small>Print Name:</small>', '');
+		$html .= $this->_getPaymentSummaryRow('<small>Signature:</small>', '');
+		$html .= $this->_getPaymentSummaryRow('<small>Date:</small>', '');
+		return $html;
+	}
+	private function _getPaymentSummaryRow($title, $content, $class='')
+	{
+		$html = '<div class="print-row ' . $class . '">';
+			$html .= '<span class="details_title inlineblock">';
+				$html .= $title;
+			$html .= '</span>';
+			$html .= '<span class="details_content inlineblock">';
+				$html .= $content;
+			$html .= '</span>';
+		$html .= '</div>';
+		return $html;
+	}
+}
+?>
