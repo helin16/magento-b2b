10bcca22235aa4b47fe1f04db74f7ae25e489359
Frank <tlan16@sina.com>
2014-12-21 00:32:51 +1100

    no html2pdf

--- core/main/bootstrap.php
+++ core/main/bootstrap.php
@@ -54,7 +54,7 @@ spl_autoload_register(array('SystemCoreAbstract','autoload'));
 // Bootstrap the Prado framework
 require_once dirname(__FILE__) . '/../3rdParty/PHPExcel/Classes/PHPExcel.php';
 require_once dirname(__FILE__) . '/../3rdParty/PHPMailer/PHPMailerAutoload.php';
-require_once dirname(__FILE__) . '/../3rdParty/HTML2PDF/html2pdf.class.php';
+// require_once dirname(__FILE__) . '/../3rdParty/HTML2PDF/html2pdf.class.php';
 require_once dirname(__FILE__) . '/../3rdParty/framework/prado.php';
 
 ?>
