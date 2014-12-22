f27ce14d48e3df4b946fea5841e44b4826cffef1
lhe <lhe@bytecraft.com.au>
2014-12-22 12:04:15 +1100

    testing pdf

--- web/test.php
+++ web/test.php
@@ -4,12 +4,20 @@ require_once 'bootstrap.php';
 
 echo '<pre>';
 Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
-$wsdl = 'http://budgetpc.com.au/api/v2_soap?wsdl=1';
-$connector = CatelogConnector::getConnector(B2BConnector::CONNECTOR_TYPE_CATELOG,
-// 		SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_WSDL),
-		$wsdl, 
-		SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_USER),
-		SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_KEY));
-$connector->importProducts();
-var_dump($productInfo);
+
+// convert in PDF
+    try
+    {
+    	
+        $html2pdf = new HTML2PDF('P', 'A4', 'en');
+//      $html2pdf->setModeDebug();
+        $html2pdf->setDefaultFont('Arial');
+        $html2pdf->writeHTML('<page>test</page>', isset($_GET['vuehtml']));
+        ob_end_clean(); //add this line here
+        $html2pdf->Output('exemple00.pdf');
+    }
+    catch(HTML2PDF_exception $e) {
+        echo $e;
+        exit;
+    }
 echo 'DONE';
