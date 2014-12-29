62d7135b9e57eec7b0d407fc0749e5cd9ede90bd
Frank Lan <Frank-Laptop@Frank-Laptop-PC>
2014-12-29 11:13:39 +1100

    db dump

--- core/promotion/Archive/db_dump.bat
+++
@@ -1,4 +0,0 @@
-:: Dump
-del .\bpcinternal_30Aug14\bpcinternal_30Aug14.sql
-c:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump.exe -h localhost -u root -proot bpcinternal > .\bpcinternal_30Aug14\bpcinternal_30Aug14.sql
-Pause
