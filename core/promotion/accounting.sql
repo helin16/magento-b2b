aeb6ed86c5d2d8d832bd68fb79c072afdcc87643
Frank Lan <Frank-Laptop@Frank-Laptop-PC>
2014-12-29 19:20:55 +1100

    accounting db structure sql

---
+++ core/promotion/accounting.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `product` ADD `totalInPartsValue` DOUBLE(10,4) NOT NULL DEFAULT '0' AFTER `totalOnHandValue`, ADD INDEX (`totalInPartsValue`) ;
+ALTER TABLE `product` ADD `stockInParts` INT(10) NOT NULL DEFAULT '0' AFTER `stockOnPO`, ADD INDEX (`stockInParts`) ;
+ALTER TABLE `product` CHANGE `invenAccNo` `assetAccNo` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+ALTER TABLE `product` DROP INDEX `invenAccNo`;
+ALTER TABLE `product` ADD INDEX(`assetAccNo`);
+ALTER TABLE `product` ADD `revenueAccNo` VARCHAR(10) NULL DEFAULT '' AFTER `assetAccNo`, ADD INDEX (`revenueAccNo`) ;
+ALTER TABLE `product` CHANGE `assetAccNo` `assetAccNo` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+ALTER TABLE `product` ADD `costAccNo` VARCHAR(10) NOT NULL DEFAULT '' AFTER `revenueAccNo`, ADD INDEX (`costAccNo`) ;
