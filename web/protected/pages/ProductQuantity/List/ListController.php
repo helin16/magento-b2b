9e0278e786f3a6e873b4479bc642446b68b8295e
Frank <tlan16@sina.com>
2014-12-30 20:35:27 +1100

    Product Qty page

--- web/protected/pages/ProductQuantity/List/ListController.php
+++ web/protected/pages/ProductQuantity/List/ListController.php
@@ -1,77 +1,80 @@
 <?php
 /**
- * This is the listing page for manufacturer
+ * This is the OrderController
  * 
  * @package    Web
  * @subpackage Controller
  * @author     lhe<helin16@gmail.com>
  */
-class ListController extends CRUDPageAbstract
+class ListController extends BPCPageAbstract
 {
 	/**
 	 * (non-PHPdoc)
 	 * @see BPCPageAbstract::$menuItem
 	 */
-	public $menuItem = 'system.messages';
-	protected $_focusEntity = 'Message';
-	/**
-	 * constructor
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		if(!AccessControl::canAccessProductsPage(Core::getRole()))
-			die('You do NOT have access to this page');
-	}
+	public $menuItem = 'purchaseorders.receiving';
 	/**
 	 * (non-PHPdoc)
-	 * @see CRUDPageAbstract::_getEndJs()
+	 * @see BPCPageAbstract::onLoad()
+	 */
+	public function onLoad($param)
+	{
+		parent::onLoad($param);
+	}
+	/**
+	 * Getting The end javascript
+	 *
+	 * @return string
 	 */
 	protected function _getEndJs()
 	{
 		$js = parent::_getEndJs();
 		$js .= "pageJs";
-		$js .= "._bindSearchKey()";
-		$js .= ".getResults(true, " . $this->pageSize . ");";
+			$js .= ".setHTMLIDs('detailswrapper','search_panel','products_table','barcode_input')";
+			$js .= ".setCallbackId('searchProduct', '" . $this->searchProductBtn->getUniqueID() . "')";
+			$js .= ".setCallbackId('saveQuantities', '" . $this->saveQuantitiesBtn->getUniqueID() . "')";
+			$js .= ".init();";
 		return $js;
 	}
-	/**
-	 * Getting the items
-	 *
-	 * @param unknown $sender
-	 * @param unknown $param
-	 * @throws Exception
-	 *
-	 */
-	public function getItems($sender, $param)
+	public function searchProduct($sender, $param)
 	{
 		$results = $errors = array();
 		try
 		{
-			$class = trim($this->_focusEntity);
-			$pageNo = 1;
-			$pageSize = DaoQuery::DEFAUTL_PAGE_SIZE;
-			if(isset($param->CallbackParameter->pagination))
-			{
-				$pageNo = $param->CallbackParameter->pagination->pageNo;
-				$pageSize = $param->CallbackParameter->pagination->pageSize;
-			}
+			$items = array();
+			$searchTxt = isset($param->CallbackParameter->searchTxt) ? trim($param->CallbackParameter->searchTxt) : '';
 			
-			$serachCriteria = isset($param->CallbackParameter->searchCriteria) ? json_decode(json_encode($param->CallbackParameter->searchCriteria), true) : array();
+			$where = 'pro_pro_code.code = :searchExact or pro.name like :searchTxt OR sku like :searchTxt';
+			$params = array('searchExact' => $searchTxt , 'searchTxt' => '%' . $searchTxt . '%');
 				
-			$where = array(1);
-			$params = array();
-			if(isset($serachCriteria['msg.to']) && ($type = trim($serachCriteria['msg.to'])) !== '')
+			$searchTxtArray = StringUtilsAbstract::getAllPossibleCombo(StringUtilsAbstract::tokenize($searchTxt));
+			if(count($searchTxtArray) > 1)
 			{
-				$where[] = 'msg.to like ?';
-				$params[] = '%' . $type . '%';
+				foreach($searchTxtArray as $index => $comboArray)
+				{
+					$key = 'combo' . $index;
+					$where .= ' OR pro.name like :' . $key;
+					$params[$key] = '%' . implode('%', $comboArray) . '%';
+				}
 			}
-			$stats = array();
-			$objects = $class::getAllByCriteria(implode(' AND ', $where), $params, false, $pageNo, $pageSize, array('msg.id' => 'asc'), $stats);
-			$results['pageStats'] = $stats;
-			$results['items'] = array();
-			foreach($objects as $obj)
-				$results['items'][] = $obj->getJson();
+			Product::getQuery()->eagerLoad('Product.codes', 'left join');
+			$products = Product::getAllByCriteria($where, $params, true, 1, DaoQuery::DEFAUTL_PAGE_SIZE, array('pro.sku' => 'asc'));
+			
+			foreach($products as $product)
+			{
+				if(!$product instanceof Product)
+					throw new Exception('Invalid Product passed in!');
+				$EANcodes = ProductCode::getAllByCriteria('pro_code.productId = :productId and pro_code.typeId = :typeId', array('productId'=> $product->getId(), 'typeId'=> ProductCodeType::ID_EAN), true, 1, 1);
+				$EANcodes = count($EANcodes) > 0 ? $EANcodes[0]->getCode() : '';
+				
+				$UPCcodes = ProductCode::getAllByCriteria('pro_code.productId = :productId and pro_code.typeId = :typeId', array('productId'=> $product->getId(), 'typeId'=> ProductCodeType::ID_UPC), true, 1, 1);
+				$UPCcodes = count($UPCcodes) > 0 ? $UPCcodes[0]->getCode() : '';
+				
+				$array = $product->getJson();
+				$array['codes'] = array('EAN'=>$EANcodes, 'UPC'=>$UPCcodes);
+				$items[] = $array;
+			}
+			$results['items'] = $items;
 		}
 		catch(Exception $ex)
 		{
@@ -80,38 +83,45 @@ class ListController extends CRUDPageAbstract
 		$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
 	}
 	/**
-	 * save the items
+	 * saveOrder
 	 *
 	 * @param unknown $sender
 	 * @param unknown $param
+	 *
 	 * @throws Exception
 	 *
 	 */
-	public function saveItem($sender, $param)
+	public function saveQuantities($sender, $param)
 	{
 		$results = $errors = array();
 		try
 		{
-			$class = trim($this->_focusEntity);
-			if(!isset($param->CallbackParameter->item))
-				throw new Exception("System Error: no item information passed in!");
-			$item = (isset($param->CallbackParameter->item->id) && ($item = $class::get($param->CallbackParameter->item->id)) instanceof $class) ? $item : null;
-			$name = trim($param->CallbackParameter->item->name);
-			$description = trim($param->CallbackParameter->item->description);
-			if($item instanceof $class)
+			var_dump($param->CallbackParameter);
+			Dao::beginTransaction();
+			$items = array();
+			$comment = trim($param->CallbackParameter->comments);
+			foreach ($param->CallbackParameter->products as $item)
 			{
-				$item->setName($name)
-					->setDescription($description)
-					->save();
+				if(!($product = Product::get(trim($item->id))) instanceof Product)
+					throw new Exception('Invalid Product passed in!');
+				if(($stockOnPO = trim($item->stockOnPO)) !== $product->getstockOnPO())
+					$product->setStockOnPO($stockOnPO);
+				if(($stockOnHand = trim($item->stockOnHand)) !== $product->getStockOnHand())
+					$product->setStockOnHand($stockOnHand);
+				if(($stockOnOrder = trim($item->stockOnOrder)) !== $product->getStockOnOrder())
+					$product->setStockOnOrder($stockOnOrder);
+				if(($stockInRMA = trim($item->stockInRMA)) !== $product->getStockInRMA())
+					$product->setStockInRMA($stockInRMA);
 			}
-			else
-			{
-				$item = $class::create($name, $description, false);
-			}
-			$results['item'] = $item->getJson();
+			$product->addComment(Comments::TYPE_WAREHOUSE, $comment)->save();
+			
+			$results['item'] = $product->getJson();
+			
+			Dao::commitTransaction();
 		}
 		catch(Exception $ex)
 		{
+			Dao::rollbackTransaction();
 			$errors[] = $ex->getMessage();
 		}
 		$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
