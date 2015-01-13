eba1187f5795c88ec91d0417e4d7cb8881f63048
lhe <lhe@bytecraft.com.au>
2015-01-13 17:04:28 +1100

    added HTML2PDF back

--- web/test.php
+++ web/test.php
@@ -9,10 +9,20 @@ Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
     try
     {
     	
-        $html2pdf = new HTML2PDF('P', 'A4', 'en');
+        $html2pdf = new HTML2PDF('P', 'A4', 'en', true, 'UTF-8');
 //      $html2pdf->setModeDebug();
-        $html2pdf->setDefaultFont('Arial');
-        $html2pdf->writeHTML('<page>test</page>', isset($_GET['vuehtml']));
+        $html = ComScriptCURL::readUrl('http://localhost:8081/', null, array(), '', array(CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_COOKIE => session_name() . '=' . md5(session_id()),
+				CURLOPT_COOKIEJAR => session_name() . '=' . md5(session_id())
+		));
+        $dom = new simple_html_dom();
+        $dom->load($html);
+        foreach($dom->find('script') as $script)
+        	$script->outertext = '';
+        $dom->load($dom->save());
+//         echo '<textarea>' . $dom->save() . '</textarea>';die;
+        echo $dom->save();die;
+        $html2pdf->writeHTML('<page>' . $dom->save() . '</page>', false);
         ob_end_clean(); //add this line here
         $html2pdf->Output('exemple00.pdf');
     }
