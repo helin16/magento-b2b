c2b54589739a5875720aeecf34e77544a4dd73e3
lin <helin16@gmail.com>
2015-01-14 22:44:49 +1100

    changed getXML for the xero connector

--- core/main/utils/connector/xero/XeroConnector_Account.php
+++ core/main/utils/connector/xero/XeroConnector_Account.php
@@ -30,7 +30,7 @@ class XeroConnector_Account extends XeroConnectorAbstract
 	{
 		$auth = $this->_getOAuth();
 		
-		$xml = $this->_constructXML($this->_xmlType, $input);
+		$xml = self::getXML($this->_xmlType, $input);
 		$xml = $this->_removeXMLHeader($xml);
 		
 		$auth->request('POST', $auth->url('Accounts', 'core'), $params, $xml);
@@ -62,5 +62,35 @@ class XeroConnector_Account extends XeroConnectorAbstract
 			$output['Order'] = trim($params['order']);
 		
 		return $output;
+	}
+	/**
+	 * get the xml for that entity
+	 * 
+	 * @param array $input
+	 * 
+	 * @return SimpleXMLElement
+	 */
+	public static function getXML(array $input = array())
+	{
+		if(!isset($input['code']) || trim($input['code']) === '' || !isset($input['name']) || trim($input['name']) === '' || !isset($input['type']) || trim($input['type']) === '')
+			throw new Exception("Mandatory element(s) missing for constructing XML for ACCOUNT");
+			
+		$xml = parent::getXML();
+		$xml->Code = trim($input['code']);
+		$xml->Name = trim($input['name']);
+		$xml->Type = trim($input['type']);
+		
+		if(isset($input['description']) && ($description = trim($input['description'])) !== '')
+			$xml->Description = $description;
+		
+		if(isset($input['tax_type']) && ($taxType = trim($input['tax_type'])) !== '')
+			$xml->TaxType = $taxType;
+		
+		if(isset($input['enable_payments_to_account']) && is_bool($input['enable_payments_to_account']))
+			$xml->EnablePaymentsToAccount = $input['enable_payments_to_account'];
+			
+		if(isset($input['show_in_expense_claims']) && is_bool($input['show_in_expense_claims']))
+			$xml->ShowInExpenseClaims = $input['show_in_expense_claims'];
+		return $xml;
 	}
 }
--- core/main/utils/connector/xero/XeroConnector_Invoice.php
+++ core/main/utils/connector/xero/XeroConnector_Invoice.php
@@ -18,4 +18,34 @@ class XeroConnector_Invoice extends XeroConnectorAbstract
 		$response = $auth->parseResponse($auth->response['response'], $auth->response['format']);
 		return isset($response->Invoices) ? $response->Invoices : null;
 	}
+	/**
+	 * get the xml for that entity
+	 *
+	 * @param array $input
+	 *
+	 * @return SimpleXMLElement
+	 */
+	public static function getXML(array $input = array())
+	{
+// 		if(!isset($input['code']) || trim($input['code']) === '' || !isset($input['name']) || trim($input['name']) === '' || !isset($input['type']) || trim($input['type']) === '')
+// 			throw new Exception("Mandatory element(s) missing for constructing XML for ACCOUNT");
+			
+// 		$xml = parent::getXML();
+// 		$xml->Code = trim($input['code']);
+// 		$xml->Name = trim($input['name']);
+// 		$xml->Type = trim($input['type']);
+	
+// 		if(isset($input['description']) && ($description = trim($input['description'])) !== '')
+// 			$xml->Description = $description;
+	
+// 		if(isset($input['tax_type']) && ($taxType = trim($input['tax_type'])) !== '')
+// 			$xml->TaxType = $taxType;
+	
+// 		if(isset($input['enable_payments_to_account']) && is_bool($input['enable_payments_to_account']))
+// 			$xml->EnablePaymentsToAccount = $input['enable_payments_to_account'];
+			
+// 		if(isset($input['show_in_expense_claims']) && is_bool($input['show_in_expense_claims']))
+// 			$xml->ShowInExpenseClaims = $input['show_in_expense_claims'];
+// 		return $xml;
+	}
 }
