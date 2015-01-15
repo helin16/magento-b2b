9dfb3c9a2054bebe1b17e21091b1fb7828d80803
Frank <tlan16@sina.com>
2015-01-15 12:08:31 +1100

    Xero: item: use parent function

--- core/main/utils/connector/xero/XeroConnector_Item.php
+++ core/main/utils/connector/xero/XeroConnector_Item.php
@@ -15,7 +15,7 @@ class XeroConnector_Item extends XeroConnectorAbstract
 	 * @return SimpleXMLElement
 	 */
 	private function _getFakeXml($code, $description = '', $salesUnitPrice = 0, $salesAccountCode = 0, $purchaseUnitPrice = 0, $purchaseAccountCode = 0, $purchaseTaxType = '') {
-		$xml = new SimpleXMLElement ( '<' . 'Item' . '/>' );
+		$xml = $this->getXML();
 		$xml->Code = $code;
 		if(isset($description))
 			$xml->Description = $description;
@@ -36,16 +36,6 @@ class XeroConnector_Item extends XeroConnectorAbstract
 				$xml->PurchaseDetails->TaxType = $purchaseTaxType;
 		}
 		return $xml;
-	}
-	/**
-	 * remove the xml header (version ...)
-	 * 
-	 * @param SimpleXMLElement $xml
-	 */
-	private function removeXMLHeader(SimpleXMLElement $xml)
-	{
-		$dom = dom_import_simplexml($xml);
-		return $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
 	}
 	/**
 	 * Getting the items
@@ -83,7 +73,7 @@ class XeroConnector_Item extends XeroConnectorAbstract
 	{
 		$auth = $this->_getOAuth();
 			$xml = $this->_getFakeXml($code, $description, $salesUnitPrice, $salesAccountCode, $purchaseUnitPrice, $purchaseAccountCode, $purchaseTaxType);
-			$xml = $this->removeXMLHeader($xml);
+			$xml = $this->_removeXMLHeader($xml);
 			$auth->request('POST', $auth->url('Items', 'core'), $params, $xml);
 			if (intval($auth->response['code']) !== self::RESPONSE_CODE_SUCCESS)
 				throw new Exception('Error' .  $auth->response['response']);
@@ -162,35 +152,4 @@ class XeroConnector_Item extends XeroConnectorAbstract
 	{
 		return $this->getItems(array('Where' => 'Code=="' . trim($value) . '"', 'order' => 'ItemID'));
 	}
-	/**
-	 * get the xml for that entity
-	 *
-	 * @param array $input
-	 *
-	 * @return SimpleXMLElement
-	 */
-	public static function getXML(array $input = array())
-	{
-// 		if(!isset($input['code']) || trim($input['code']) === '' || !isset($input['name']) || trim($input['name']) === '' || !isset($input['type']) || trim($input['type']) === '')
-// 			throw new Exception("Mandatory element(s) missing for constructing XML for ACCOUNT");
-			
-// 		$xml = parent::getXML();
-// 		$xml->Code = trim($input['code']);
-// 		$xml->Name = trim($input['name']);
-// 		$xml->Type = trim($input['type']);
-	
-// 		if(isset($input['description']) && ($description = trim($input['description'])) !== '')
-// 			$xml->Description = $description;
-	
-// 		if(isset($input['tax_type']) && ($taxType = trim($input['tax_type'])) !== '')
-// 			$xml->TaxType = $taxType;
-	
-// 		if(isset($input['enable_payments_to_account']) && is_bool($input['enable_payments_to_account']))
-// 			$xml->EnablePaymentsToAccount = $input['enable_payments_to_account'];
-			
-// 		if(isset($input['show_in_expense_claims']) && is_bool($input['show_in_expense_claims']))
-// 			$xml->ShowInExpenseClaims = $input['show_in_expense_claims'];
-// 		return $xml;
-	}
-	
 }
