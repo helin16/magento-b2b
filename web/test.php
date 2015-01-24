8546a1c82bfe755d7d9d03da953d521dda7d27ad
Frank Lan <Frank-Laptop@Frank-Laptop-PC>
2015-01-24 16:26:01 +1100

    test

--- web/test.php
+++ web/test.php
@@ -3,7 +3,7 @@
 require_once 'bootstrap.php';
 Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
 $pdf1 = EntityToPDF::getPDF(Order::get(4290), 'docket');
-$pdf2 = EntityToPDF::getPDF(PurchaseOrder::get(4));
+// $pdf2 = EntityToPDF::getPDF(PurchaseOrder::get(4));
 // We'll be outputting a PDF
 // header('Content-Type: application/pdf');
 var_dump($pdf1);
