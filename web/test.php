d52594564f1bd709fdbe7b2953df00f9c330f4b3
Frank Lan <Frank-Laptop@Frank-Laptop-PC>
2015-01-24 16:32:30 +1100

    test

--- web/test.php
+++ web/test.php
@@ -2,12 +2,14 @@
 
 require_once 'bootstrap.php';
 Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
-$pdf1 = EntityToPDF::getPDF(Order::get(4290), 'docket');
-// $pdf2 = EntityToPDF::getPDF(PurchaseOrder::get(4));
+$pdf1 = EntityToPDF::getPDF(Order::get(10376));
+$pdf2 = EntityToPDF::getPDF(Order::get(10376), 'docket');
+$pdf3 = EntityToPDF::getPDF(PurchaseOrder::get(4));
 // We'll be outputting a PDF
 // header('Content-Type: application/pdf');
 var_dump($pdf1);
 var_dump($pdf2);
+var_dump($pdf3);
 // The PDF source is in original.pdf
 // readfile($pdf)
 ?>
