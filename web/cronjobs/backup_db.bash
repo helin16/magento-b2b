8d6c22ebd3c13c4a56521bd62af2c8dd290d3eb0
Frank Lan <Frank-Laptop@Frank-Laptop-PC>
2015-01-31 16:57:48 +1100

    db backup script bug fixed

--- web/cronjobs/backup_db.bash
+++ web/cronjobs/backup_db.bash
@@ -1,4 +1,3 @@
-cd /tmp/;
 mysqldump -uroot -proot bpcinternal > /tmp/`date +'%d_%m_%Y'`.sql;
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on /tmp/`date +'%d_%m_%Y'`.7z /tmp/`date +'%d_%m_%Y'`.sql;
 rm -f /tmp/`date +'%d_%m_%Y'`.sql;
