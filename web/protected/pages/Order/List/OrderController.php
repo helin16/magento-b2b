c23bea164a292c17e34fb35967c5893f872feb61
lin <helin16@gmail.com>
2015-02-26 08:04:28 +1100

    again

--- web/protected/pages/Order/List/OrderController.php
+++ web/protected/pages/Order/List/OrderController.php
@@ -142,7 +142,7 @@ class OrderController extends BPCPageAbstract
 					}
 					case 'ord.infos.' . OrderInfoType::ID_CUS_NAME:
 					{
-						$query->eagerLoad("Order.customer", 'inner join', 'x', 'x.customerId = ord.id and x.active = 1');
+						$query->eagerLoad("Order.customer", 'inner join', 'x', 'x.id = ord.customerId and x.active = 1');
 						$where[] = 'x.name like ?';
 						$params[] = '%' . $value.'%';
 						break;
