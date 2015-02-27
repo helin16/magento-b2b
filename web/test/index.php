5a725669f353313d3531780efd91e18c2542afe8
lin <helin16@gmail.com>
2015-02-27 20:56:54 +1100

    again

--- web/test/index.php
+++ web/test/index.php
@@ -1,2 +1,12 @@
 <?php
 require_once dirname(__FILE__) . '/src/Pdf.php';
+
+use mikehaertl\wkhtmlto\Pdf;
+
+// You can pass a filename, a HTML string or an URL to the constructor
+$pdf = new Pdf('http://google.com.au');
+
+// On some systems you may have to set the binary path.
+// $pdf->binary = 'C:\...';
+
+$pdf->saveAs('/tmp/new.pdf');
