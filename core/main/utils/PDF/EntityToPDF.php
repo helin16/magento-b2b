e3df91a7733e8c454306c8394e92b5b319aa05bc
lin <helin16@gmail.com>
2015-02-27 20:46:13 +1100

    no js

--- core/main/utils/PDF/EntityToPDF.php
+++ core/main/utils/PDF/EntityToPDF.php
@@ -42,12 +42,12 @@ class EntityToPDF
 		}
 		$url .= "?user=" . Core::getUser()->getUserName() . '&pass=' . Core::getUser()->getPassword();
 		$url = 'http://' . $_SERVER["HTTP_HOST"] . '/' . $url ;
-		$command = '/usr/local/bin/wkhtmltopdf -B 0 -T 0 --disable-javascript "' . $url . '" ' . ($file = '/tmp/' . md5(new UDate()) . '.pdf');
+		$command = '/usr/local/bin/wkhtmltopdf -B 5mm -T 5mm --javascript-delay 1000 --page-size A4 --encoding utf-8 "' . $url . '" ' . ($file = '/tmp/' . md5(new UDate()) . '.pdf');
 		$output = '';
 		exec($command, $output);
-		sleep(1);
+		sleep(2);
 		if(!is_file($file))
-			throw new Exception('Could NOT generate pdf @' . $file . ' with command:' . $command . ' Output: ' . print_r($output, true));
+			throw new Exception('Could NOT generate pdf @' . $file . ' with command:' . $command);
 		return $file;
 	}
 }
