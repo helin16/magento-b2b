3b6917f6b6ce734e66e0f8ebb740b2faccf3c712
lhe <lhe@bytecraft.com.au>
2015-03-02 13:22:20 +1100

    remove debug code

--- web/protected/controls/OrderBtns/OrderBtns.php
+++ web/protected/controls/OrderBtns/OrderBtns.php
@@ -11,7 +11,7 @@ class OrderBtns extends TTemplateControl
 	public function onInit($param)
 	{
 		parent::onInit($param);
-		
+
 		$scriptArray = BPCPageAbstract::getLastestJS(get_class($this));
 		foreach($scriptArray as $key => $value)
 		{
@@ -24,7 +24,7 @@ class OrderBtns extends TTemplateControl
 			}
 		}
 	}
-	
+
 	public function onLoad($param)
 	{
 		parent::onLoad($param);
@@ -48,7 +48,7 @@ class OrderBtns extends TTemplateControl
 		try
 		{
 			Dao::beginTransaction();
-	
+
 			if(!isset($param->CallbackParameter->orderId) || !($order = Order::get($param->CallbackParameter->orderId)) instanceof Order)
 				throw new Exception('System Error: invalid order provided!');
 			if(!isset($param->CallbackParameter->emailAddress) || ($emailAddress = trim($param->CallbackParameter->emailAddress)) === '')
@@ -56,13 +56,13 @@ class OrderBtns extends TTemplateControl
 			$emailBody = '';
 			if(isset($param->CallbackParameter->emailBody) && ($emailBody = trim($param->CallbackParameter->emailBody)) !== '')
 				$emailBody = str_replace("\n", "<br />", $emailBody);
-	
-// 			$pdfFile = EntityToPDF::getPDF($order);
-// 			$asset = Asset::registerAsset($order->getOrderNo() . '.pdf', file_get_contents($pdfFile), Asset::TYPE_TMP);
-			EmailSender::addEmail('sales@budgetpc.com.au', $emailAddress, 'BudgetPC Order:' . $order->getOrderNo() , (trim($emailBody) === '' ? '' : $emailBody . "<br /><br />") .'Please find attached Order (' . $order->getOrderNo() . ') from Budget PC Pty Ltd.', array());
-// 			$order->addComment('An email sent to "' . $emailAddress . '" with the attachment: ' . $asset->getAssetId(), Comments::TYPE_SYSTEM);
+
+			$pdfFile = EntityToPDF::getPDF($order);
+			$asset = Asset::registerAsset($order->getOrderNo() . '.pdf', file_get_contents($pdfFile), Asset::TYPE_TMP);
+			EmailSender::addEmail('sales@budgetpc.com.au', $emailAddress, 'BudgetPC Order:' . $order->getOrderNo() , (trim($emailBody) === '' ? '' : $emailBody . "<br /><br />") .'Please find attached Order (' . $order->getOrderNo() . ') from Budget PC Pty Ltd.', array($asset));
+			$order->addComment('An email sent to "' . $emailAddress . '" with the attachment: ' . $asset->getAssetId(), Comments::TYPE_SYSTEM);
 			$results['item'] = $order->getJson();
-	
+
 			Dao::commitTransaction();
 		}
 		catch(Exception $ex)
