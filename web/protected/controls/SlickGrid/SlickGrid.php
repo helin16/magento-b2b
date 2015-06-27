4db8060b047d4719b880a74c45699aeb3220f362
frank <tlan16@sina.com>
2015-06-27 18:00:42 +1000

    new control: slick grid

---
+++ web/protected/controls/SlickGrid/SlickGrid.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The SlickGrid Loader
+ *
+ * @package    web
+ * @subpackage controls
+ * @author     lhe<helin16@gmail.com>
+ */
+class SlickGrid extends TClientScript
+{
+	/**
+	 * (non-PHPdoc)
+	 * @see TControl::onLoad()
+	 */
+	public function onLoad($param)
+	{
+		if(!$this->getPage()->IsPostBack || !$this->getPage()->IsCallback)
+		{
+			$clientScript = $this->getPage()->getClientScript();
+			$folder = $this->publishFilePath(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR);
+			// Add jQuery library
+			// Add mousewheel plugin (this is optional)
+			$clientScript->registerHeadScriptFile('SlickGrid.core.js', $folder . '/slick.core.js');
+			$clientScript->registerHeadScriptFile('SlickGrid.js', $folder . '/slick.grid.js');
+			$clientScript->registerStyleSheetFile('SlickGrid.css', $folder . '/slick.grid.css', 'screen');
+		}
+	}
+}
