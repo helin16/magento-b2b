433804c79a769e10a65c17d05702541e07d1f398
frank <tlan16@sina.com>
2015-06-27 22:30:52 +1000

    control: slickGrid bug fixed

--- web/protected/controls/SlickGrid/SlickGrid.php
+++ web/protected/controls/SlickGrid/SlickGrid.php
@@ -20,6 +20,7 @@ class SlickGrid extends TClientScript
 			$folder = $this->publishFilePath(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR);
 			// Add jQuery library
 			// Add mousewheel plugin (this is optional)
+			$clientScript->registerHeadScriptFile('SlickGrid.jQuery.drag.js', $folder . '/lib/jquery.event.drag-2.2.js');
 			$clientScript->registerHeadScriptFile('SlickGrid.core.js', $folder . '/slick.core.js');
 			$clientScript->registerHeadScriptFile('SlickGrid.js', $folder . '/slick.grid.js');
 			$clientScript->registerStyleSheetFile('SlickGrid.css', $folder . '/slick.grid.css', 'screen');
