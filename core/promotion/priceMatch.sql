4178066396290dbe3bac1f5b57a69f0a2ba29e5f
Frank <tlan16@sina.com>
2015-07-06 00:03:45 +1000

    new sql

--- core/promotion/priceMatch.sql
+++ core/promotion/priceMatch.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS `pricematchmin`;
 CREATE TABLE `pricematchmin` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`recordId` int(10) unsigned NULL DEFAULT NULL,
 	`sku` varchar(50) NOT NULL DEFAULT '',
+	`recordId` int(10) unsigned NULL DEFAULT NULL,
 	`active` bool NOT NULL DEFAULT 1,
 	`created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
 	`createdById` int(10) unsigned NOT NULL DEFAULT 0,
@@ -19,7 +19,7 @@ CREATE TABLE `pricematchrecord` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`minId` int(10) unsigned NOT NULL DEFAULT 0,
 	`companyId` int(10) unsigned NOT NULL DEFAULT 0,
-	`url` varchar(50) NOT NULL DEFAULT '',
+	`url` varchar(255) NOT NULL DEFAULT '',
 	`price` double(10,4) unsigned NOT NULL DEFAULT 0,
 	`name` varchar(100) NOT NULL DEFAULT '',
 	`active` bool NOT NULL DEFAULT 1,
