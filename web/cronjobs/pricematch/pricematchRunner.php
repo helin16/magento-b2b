2ca8c04306bf26ec2adacf68bef169ff04b62201
Frank <Frank@Frank_BPC_PC>
2015-07-08 13:57:41 +1000

    bug fix

--- web/cronjobs/pricematch/pricematchRunner.php
+++ web/cronjobs/pricematch/pricematchRunner.php
@@ -3,7 +3,7 @@ require_once dirname(__FILE__) . '/../../bootstrap.php';
 
 Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
 
-$productIds = Dao::getResultsNative('select distinct id from product where active = 1 and id > 42556', array(), PDO::FETCH_ASSOC);
+$productIds = Dao::getResultsNative('select distinct id from product where active = 1', array(), PDO::FETCH_ASSOC);
 
 foreach ($productIds as $row)
 {
