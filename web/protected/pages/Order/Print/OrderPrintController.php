24681b7d589b58a3ddf3a2dc1a683c6799cb1189
Frank <Frank@Frank_BPC_PC>
2015-07-09 16:52:24 +1000

    printing pages customer name postition

--- web/protected/pages/Order/Print/OrderPrintController.php
+++ web/protected/pages/Order/Print/OrderPrintController.php
@@ -114,10 +114,13 @@ class OrderPrintController extends BPCPageAbstract
 		$address = $this->order->$method();
 		if(!$address instanceof Address)
 			return '';
-		$html = $address->getContactName() . '<br />';
+		$html = '';
+		if(trim($this->order->getCustomer()->getName()) !== trim($address->getContactName()))
+			$html .= $this->order->getCustomer()->getName() . '<br />';
+		$html .= $address->getContactName() . '<br />';
 		$html .= $address->getStreet() . '<br />';
 		$html .= $address->getCity() . ' ' . $address->getRegion() . ' ' . $address->getPostCode() . '<br />';
-		$html .= $address->getCountry();
+// 		$html .= $address->getCountry();
 		return $html;
 	}
 	public function getPaymentSummary()
