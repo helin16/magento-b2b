07c30dd64c79c8e8785c2c7358c3ed9d3658b3aa
Frank <tlan16@sina.com>
2015-07-11 19:13:45 +1000

    adjast neg stock on hand script

---
+++ core/promotion/Archive/setNegStockOnHandToZero.php
@@ -0,0 +1,21 @@
+<?php
+require_once dirname(__FILE__) . '/../main/bootstrap.php';
+try {
+	echo "Begin" . "\n";
+	Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
+	Dao::beginTransaction();
+	
+	foreach (Product::getAllByCriteria('stockOnHand < 0') as $product)
+	{
+		echo $product->getSku() . "\n";
+		$product->setStockOnOrder(0)->snapshotQty(null,ProductQtyLog::TYPE_STOCK_ADJ,'stock adjustment for negative stock on hand')->save();
+	}
+
+	Dao::commitTransaction();
+} catch (Exception $e)
+{ 
+	echo $e->getTraceAsString();
+	Dao::rollbackTransaction();
+	throw $e;
+}
+?>
