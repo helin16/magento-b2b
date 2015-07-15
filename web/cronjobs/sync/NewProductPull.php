51fdc38441ed5db0e16fb682c873c5e4d2d07823
Frank <Frank@Frank_BPC_PC>
2015-07-15 11:50:03 +1000

    bug fix

--- web/cronjobs/sync/NewProductPull.php
+++ web/cronjobs/sync/NewProductPull.php
@@ -8,21 +8,9 @@ echo "Begin MELB TIME: " . UDate::now(UDate::TIME_ZONE_MELB) . "\n";
 
 importNewProduct();
 
-try
-{
-	Dao::beginTransaction();
-	
-	if(($systemSetting = SystemSettings::getByType(SystemSettings::TYPE_LAST_NEW_PRODUCT_PULL)) instanceof SystemSettings)
-		$systemSetting->setValue(UDate::now()->__toString())->save();
-	else throw new Exception('cannot set LAST_NEW_PRODUCT_PULL in system setting');
-	
-	Dao::commitTransaction();
-} catch (Exception $e)
-{
-	echo "\n" . $e->getMessage() . "\n" . $e->getTraceAsString() . "\n";
-	Dao::rollbackTransaction();
-	throw $e;
-}
+if(($systemSetting = SystemSettings::getByType(SystemSettings::TYPE_LAST_NEW_PRODUCT_PULL)) instanceof SystemSettings)
+	$systemSetting->setValue(UDate::now()->__toString())->save();
+else throw new Exception('cannot set LAST_NEW_PRODUCT_PULL in system setting');
 
 function importNewProduct()
 {
