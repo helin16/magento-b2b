4196aae09d385d9de64c9988ba668f719f9831b4
Frank <Frank@Frank_BPC_PC>
2015-07-15 11:52:53 +1000

    rollback last commit

--- web/cronjobs/sync/NewProductPull.php
+++ web/cronjobs/sync/NewProductPull.php
@@ -8,9 +8,21 @@ echo "Begin MELB TIME: " . UDate::now(UDate::TIME_ZONE_MELB) . "\n";
 
 importNewProduct();
 
-if(($systemSetting = SystemSettings::getByType(SystemSettings::TYPE_LAST_NEW_PRODUCT_PULL)) instanceof SystemSettings)
-	$systemSetting->setValue(UDate::now()->__toString())->save();
-else throw new Exception('cannot set LAST_NEW_PRODUCT_PULL in system setting');
+try
+{
+	Dao::beginTransaction();
+	
+	if(($systemSetting = SystemSettings::getByType(SystemSettings::TYPE_LAST_NEW_PRODUCT_PULL)) instanceof SystemSettings)
+		$systemSetting->setValue(UDate::now()->__toString())->save();
+	else throw new Exception('cannot set LAST_NEW_PRODUCT_PULL in system setting');
+	
+	Dao::commitTransaction();
+} catch (Exception $e)
+{
+	echo "\n" . $e->getMessage() . "\n" . $e->getTraceAsString() . "\n";
+	Dao::rollbackTransaction();
+	throw $e;
+}
 
 function importNewProduct()
 {
