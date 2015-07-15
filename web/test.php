29e7d524d28fa5c738aa0d7898d9e34de0fa792f
Frank <Frank@Frank_BPC_PC>
2015-07-15 10:56:07 +1000

    test.php

--- web/test.php
+++ web/test.php
@@ -1,24 +1,17 @@
 <?php
 require_once 'bootstrap.php';
 try {
-	echo "Hello<br/>";
+	echo "Begin" . "\n";
 	Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
 	Dao::beginTransaction();
-	echo '<pre>';
-
-	$product = Product::get(38762);
-	$lastPurchaseTime = UDate::now();
-	$receivingItem = ReceivingItem::get(25);
-	$comments = 'cccccccccccc';
 	
-	$newAgeingLog = ProductAgeingLog::create($product, $lastPurchaseTime, $comments);
-	var_dump($newAgeingLog->getJson());
-	echo '</pre>';
-
+	if(($systemSetting = SystemSettings::getSettings(SystemSettings::TYPE_LAST_NEW_PRODUCT_PULL)) instanceof SystemSettings)
+		$systemSetting->setValue(UDate::now())->save();
+	
 	Dao::commitTransaction();
 } catch (Exception $e)
 { 
-	echo "<pre>" . $e->getTraceAsString() . "</pre>";
+	echo "\n" . $e->getMessage() . "\n" . $e->getTraceAsString() . "\n";
 	Dao::rollbackTransaction();
 	throw $e;
 }
