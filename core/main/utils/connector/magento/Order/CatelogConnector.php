0f01ee04a2df970ea8b616fe85111b5fa6c9db7d
Frank <Frank@Frank_BPC_PC>
2015-07-18 18:03:50 +1000

    bug fix

--- core/main/utils/connector/magento/Order/CatelogConnector.php
+++ core/main/utils/connector/magento/Order/CatelogConnector.php
@@ -281,8 +281,7 @@ class CatelogConnector extends B2BConnector
 					continue;
 				// handle extra long sku from magento, exceeding mysql sku length limit
 				DaoMap::loadMap('Product');
-				$skuSizeLimit = DaoMap::$map['product']['sku'];
-				echo 'checking sku length, get' . strlen($sku) . ', limit is ' . $skuSizeLimit . "\n"; 
+				$skuSizeLimit = DaoMap::$map['product']['sku']['size'];
 				if(strlen($sku) > $skuSizeLimit)
 				{
 					echo 'Product ' . $sku . '(id=' . $product->getId() . ', magento Product Creation Time=' . trim($pro->created_at) . ') magento sku length exceed system sku length limit of' . $skuSizeLimit . ', skipped' . "\n";
