d3c3ea3cac9b11a176b90c7a65e173c8d8bcaebc
lhe <lhe@bytecraft.com.au>
2015-08-04 14:36:33 +1000

    debug code

--- web/testMagento.php
+++ web/testMagento.php
@@ -5,19 +5,34 @@ try {
 	echo "Begin" . "\n";
 	Dao::beginTransaction();
 
-	$connector = CatelogConnector::getConnector(B2BConnector::CONNECTOR_TYPE_CATELOG,
-			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_WSDL),
-			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_USER),
-			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_KEY));
-	
-	$result = $connector->importProducts(true, true);
+	$wsdl = 'http://www.budgetpc.com.au/api/v2_soap?wsdl=1';
+	$options = array('exceptions' => true, 'encoding'=>'utf-8', 'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP);
+	$options = array_merge($options, array('proxy_host' => "proxy.bytecraft.internal", 'proxy_port' => 3128));
+	$soapClient = ComScriptSoap::getScript($wsdl, $options);
+
+	$session = $soapClient->login('B2BUser', 'B2BUser');
+	$params = array(
+			'complex_filter' => array(
+					array(
+							'key' => 'created_at',
+							'value' => array(
+									'key' => 'gt',
+									'value' => '2015-08-03 00:10:02'
+							),
+					),
+			)
+	);
+	$result = $soapClient->salesOrderList($session, $params);
+	var_dump(is_soap_fault($result));
+// 	var_dump($result->faultcode);
+// 	var_dump($result->faultstring);
 // 	$result = $connector->getProductInfo('test-graphic-card');
-	
-// 	var_dump($result);
-	
+
+	var_dump($result);
+
 	Dao::commitTransaction();
 } catch (Exception $e)
-{ 
+{
 	echo "\n" . $e->getMessage() . "\n" . $e->getTraceAsString() . "\n";
 	Dao::rollbackTransaction();
 	throw $e;
