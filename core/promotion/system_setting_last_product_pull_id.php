a2867250ab5552ae51aeebd649b475c516765db5
tlan16 <tlan16@localhost>
2015-08-07 17:26:42 +1000

    bug fix

--- core/promotion/system_setting_last_product_pull_id.php
+++ core/promotion/system_setting_last_product_pull_id.php
@@ -5,8 +5,9 @@ try {
 	Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
 	Dao::beginTransaction();
 
-	SystemSettings::addSettings(SystemSettings::TYPE_LAST_PRODUCT_PULL_ID, 0);
-	
+	$new = count(SystemSettings::getAllByCriteria('type = ?', array(SystemSettings::TYPE_LAST_PRODUCT_PULL_ID), false)) > 0 ? SystemSettings::getAllByCriteria('type = ?', array(SystemSettings::TYPE_LAST_PRODUCT_PULL_ID), true)[0] : new SystemSettings();
+	$new->setType(SystemSettings::TYPE_LAST_PRODUCT_PULL_ID)->setActive(true)->setValue(1)->setDescription('last id of pull product from magento to system')->save();
+
 	Dao::commitTransaction();
 } catch (Exception $e)
 {
