a1d1a8535dc751228418598f5b3da727cac04e8f
Frank Lan <franklan118@gmail.com>
2015-08-19 17:54:01 +1000

    debug

--- web/cronjobs/sync/ProductDownload.php
+++ web/cronjobs/sync/ProductDownload.php
@@ -6,17 +6,12 @@ require_once dirname(__FILE__) . '/../../bootstrap.php';
 Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
 echo "Begin importProducts from magento MELB TIME: " . UDate::now(UDate::TIME_ZONE_MELB) . "\n";
 
-importNewProduct();
-
-echo "Done importProducts from magento MELB TIME: " . UDate::now(UDate::TIME_ZONE_MELB) . "\n";
-
-
-function importNewProduct()
-{
-	$script = CatelogConnector::getConnector(B2BConnector::CONNECTOR_TYPE_CATELOG,
+$script = CatelogConnector::getConnector(B2BConnector::CONNECTOR_TYPE_CATELOG,
 			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_WSDL),
 			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_USER),
 			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_KEY)
-	)
-	->downloadProductInfo();
-}
+	);
+$script->downloadProductInfo();
+
+echo "Done importProducts from magento MELB TIME: " . UDate::now(UDate::TIME_ZONE_MELB) . "\n";
+
