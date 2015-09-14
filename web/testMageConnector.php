110eaf5db67fd0ff9b0cc36e0999c8f4faf35124
Frank Lan <franklan118@gmail.com>
2015-09-14 21:49:17 +1000

    test

--- web/testMageConnector.php
+++ web/testMageConnector.php
@@ -1,8 +1,20 @@
 <?php
 require_once 'bootstrap.php';
 try {
-	$product = Product::getBySku('059-08268(WORD13DVD)');
-	var_dump($product->getJson(array('mageInfo'=>$product->getMageInfo()))['mageInfo']);
+	$sku = "VS247HV";
+	$param = array(
+		'newInfo' => array(
+			'price' => 189.00
+		)
+		,'newAttributes' => array(
+			'all_ln_stock' => 'Ships In 24Hrs'
+		)
+	);
+	$connector = CatelogConnector::getConnector(B2BConnector::CONNECTOR_TYPE_CATELOG,
+			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_WSDL),
+			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_USER),
+			SystemSettings::getSettings(SystemSettings::TYPE_B2B_SOAP_KEY));
+	$connector->updateProductInfo($sku, $param);
 }
 catch (Exception $e)
 {
