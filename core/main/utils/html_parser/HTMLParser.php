dbf74b3366a96628f93c640d63e5f5bdbff5532d
helin16 <helin16@192.168.200.202>
2015-10-27 19:39:55 +1100

    debug

--- core/main/utils/html_parser/HTMLParser.php
+++ core/main/utils/html_parser/HTMLParser.php
@@ -33,7 +33,7 @@ abstract class HTMLParser
 	private static function _readUrl($url)
 	{
 		$options = array(CURLOPT_USERAGENT => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
-		$data = ComScriptCURL::readUrl($url,null, $data, '', $options);
+		$data = ComScriptCURL::readUrl($url,null, array(), '', $options);
 		$dom = new simple_html_dom();
 		$dom->load($data); 
 		return $dom;
