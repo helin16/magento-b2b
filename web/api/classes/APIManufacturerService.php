7cb610b459f922e1adece95c3f39cc9db1a17a88
lin.he <lin.he@TA030593.tattsgroup.com>
2015-11-10 13:01:22 +1100

    added manufacturer and supplier for the API

---
+++ web/api/classes/APIManufacturerService.php
@@ -0,0 +1,5 @@
+<?php
+class APIManufacturerService extends APIServiceAbstract
+{
+   protected $entityName = 'Manufacturer';
+}
---
+++ web/api/classes/APISupplierService.php
@@ -0,0 +1,5 @@
+<?php
+class APISupplierService extends APIServiceAbstract
+{
+   protected $entityName = 'Supplier';
+}
