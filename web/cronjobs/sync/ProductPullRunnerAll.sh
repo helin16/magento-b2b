e5b3cf9352cfa0fee1cdf2306e39e07ce0007d43
larryu <budgetpc@192.168.1.112>
2015-11-30 15:34:35 +1100

    manufal handle log

--- web/cronjobs/sync/ProductPullRunnerAll.sh
+++ web/cronjobs/sync/ProductPullRunnerAll.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Product Attribute Set Pull
-/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductAttributeSetPull.php >> /tmp/ProductAttributeSetPull_`date +"%d_%b_%y"`.log
+/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductAttributeSetPull.php
 # Product Attribute Pull
-/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductAttributePull.php >> /tmp/ProductAttributePull_`date +"%d_%b_%y"`.log
+/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductAttributePull.php
 # Product Category Pull
-/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductCategoryPull.php >> /tmp/ProductCategoryPull_`date +"%d_%b_%y"`.log
+/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductCategoryPull.php
 # Product Manufacturer Pull
-/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductManufacturerPull.php >> /tmp/ProductManufacturerPull_`date +"%d_%b_%y"`.log
+/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductManufacturerPull.php
 # Product Download
-/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductDownloadNAll.php >> /tmp/ProductDownloadAll_`date +"%d_%b_%y"`.log
+/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductDownloadNAll.php
 # Product Process Download
-/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductProcessDownloadedAll.php >> /tmp/ProductProcessDownloadedAll.php_`date +"%d_%b_%y"`.log
+/usr/bin/php /var/www/magentob2b/web/cronjobs/sync/ProductProcessDownloadedAll.php
