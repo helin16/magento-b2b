f586414cc1aa1a9a9f262af315013a1f713e3736
larryu <budgetpc@192.168.1.103>
2016-01-11 12:43:44 +1100

    fix promotion time

--- core/promotion/PromotionController.sql
+++ core/promotion/PromotionController.sql
@@ -63,32 +63,32 @@ BEGIN
   declare $dayOfWeek int;
   SELECT DAYOFWEEK(DATE_ADD(now(), INTERVAL 11 HOUR)) into $dayOfWeek;
   if ($dayOfWeek = 7) then
-    update systemsettings set value = 1 where type = 'is_daily_promotion_time';
+    update systemsettings set value = 1 where type = 'is_weekend_promotion_time';
     UPDATE productprice pp
     INNER JOIN product_category pc
        ON pp.productId = pc.productId
     INNER JOIN product p
        ON pp.productId = p.id
     SET pp.updated = now()
-    WHERE pp.typeId = 5 and pp.active = 1 and p.active=1 and pc.active=1 and pc.categoryId = 339;
+    WHERE pp.typeId = 6 and pp.active = 1 and p.active=1 and pc.active=1 and pc.categoryId = 339;
   end if;
 END;
 
 CREATE PROCEDURE WeekendPromotionClose()
 BEGIN
-  -- close weekend promotion at 00:00:00 every Monday
-  -- 2 is Monday
+  -- close weekend promotion at 23:55:00 every Sunday
+  -- 1 is Sunday
   declare $dayOfWeek int;
   SELECT DAYOFWEEK(DATE_ADD(now(), INTERVAL 11 HOUR)) into $dayOfWeek;
-  if ($dayOfWeek = 2) then
-    update systemsettings set value = 0 where type = 'is_daily_promotion_time';
+  if ($dayOfWeek = 1) then
+    update systemsettings set value = 0 where type = 'is_weekend_promotion_time';
     UPDATE productprice pp
     INNER JOIN product_category pc
        ON pp.productId = pc.productId
     INNER JOIN product p
        ON pp.productId = p.id
     SET pp.updated = now()
-    WHERE pp.typeId = 5 and pp.active = 1 and p.active=1 and pc.active=1  and pc.categoryId = 339;
+    WHERE pp.typeId = 6 and pp.active = 1 and p.active=1 and pc.active=1  and pc.categoryId = 339;
   end if;
 END;
 
@@ -125,12 +125,12 @@ DO BEGIN
   call WeekendPromotionOpen();
 END;
 
--- weekend promotion ends from 00:00:00 (Melbourne time) every Monday
+-- weekend promotion ends from 23:55:00 (Melbourne time) every Sunday
 CREATE EVENT e_WeekendPromotionEnd ON SCHEDULE
-EVERY 1 WEEK STARTS CONCAT(CURRENT_DATE + INTERVAL 6 - WEEKDAY(CURRENT_DATE) DAY,' 13:00:00')
+EVERY 1 WEEK STARTS CONCAT(CURRENT_DATE + INTERVAL 6 - WEEKDAY(CURRENT_DATE) DAY,' 12:55:00')
 ON COMPLETION PRESERVE
 ENABLE
-COMMENT 'weekend promotion ends at 00:00:00(Melbourne time) every Monday'
+COMMENT 'weekend promotion ends at 23:55:00(Melbourne time) every Sunday'
 DO BEGIN
   call WeekendPromotionClose();
 END;
--- core/promotion/PromotionRelated.sql
+++ core/promotion/PromotionRelated.sql
@@ -6,5 +6,11 @@ update productcategory set active = 1 where id in (334, 339,335, 336, 337);
 insert into systemsettings (`type`, `value`, `description`, `active`, `created`, `createdById`, `updated`, `updatedById` ) values
     ('is_daily_promotion_time', 0, 'The flag to judge the whether the daily promotion time starts or not', 1, NOW(), 10, NOW(), 10);
 
+insert into systemsettings (`type`, `value`, `description`, `active`, `created`, `createdById`, `updated`, `updatedById` ) values
+    ('is_weekend_promotion_time', 0, 'The flag to judge the whether the daily promotion time starts or not', 1, NOW(), 10, NOW(), 10);
+
 insert into productpricetype (`name`, `description`, `needTime`, `active`, `created`, `createdById`, `updated`, `updatedById` ) values
     ('Daily Special Price', 'Promotional purpose sepcial price for every night', 0, 1, NOW(), 10, NOW(), 10);
+    
+insert into productpricetype (`name`, `description`, `needTime`, `active`, `created`, `createdById`, `updated`, `updatedById` ) values
+    ('Weekend Special Price', 'Promotional purpose sepcial price for weekend', 0, 1, NOW(), 10, NOW(), 10);
