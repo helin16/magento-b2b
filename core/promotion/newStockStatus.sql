7bb47c865b818691125cdbfea12813e7aef29c8d
Larry Yu <yuxiaofengch@hotmail.com>
2016-01-22 09:59:12 +1100

    new for status of Ships in 7 Days

--- core/promotion/newStockStatus.sql
+++ core/promotion/newStockStatus.sql
@@ -1,2 +1,5 @@
 insert into productstatus (`name`, `description`, `active`, `created`, `createdById`, `updated`, `updatedById` ) values
     ('Call for ETA', 'For products that we have no stock', 1, NOW(), 10, NOW(), 10);
+    
+insert into productstatus (`name`, `description`, `active`, `created`, `createdById`, `updated`, `updatedById` ) values
+    ('Ships in 7 Days', 'For products that we must have 7 days to ship to customers', 1, NOW(), 10, NOW(), 10);
